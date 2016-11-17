c9dd6b92cd4fba527fc8568c98a196273cef9903
Brad Chao <brad@brad.tw>
2016-11-17 13:54:01 +0800

    v3

--- app/src/main/java/com/example/user/brad06/MainActivity.java
+++ app/src/main/java/com/example/user/brad06/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.user.brad06;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void test1(View v){
+        MyThread mt1 = new MyThread();
+        mt1.start();
+    }
+    private class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int i=0; i<10; i++){
+                Log.v("brad", "i = " + i);
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
