e4bdc7a7c271fafa6fd1aa510f91b99b36b1e611
Brad Chao <brad@brad.tw>
2016-11-17 14:04:53 +0800

    v4

--- app/src/main/java/com/example/user/brad06/MainActivity.java
+++ app/src/main/java/com/example/user/brad06/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv);
     }
 
     public void test1(View v){
@@ -22,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             for (int i=0; i<10; i++){
                 Log.v("brad", "i = " + i);
+                tv.setText("i = " + i);
                 try {
-                    Thread.sleep(200);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
